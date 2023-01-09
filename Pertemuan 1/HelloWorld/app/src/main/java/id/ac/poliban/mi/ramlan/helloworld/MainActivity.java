feb221e53e99acd76d4cc2e9eb6c3a812c2e0433
fendryahyar <122244894+fendryahyar@users.noreply.github.com>
2023-01-10 02:09:47 +0800

    Pertemuan 1

---
+++ Pertemuan 1/HelloWorld/app/src/main/java/id/ac/poliban/mi/ramlan/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package id.ac.poliban.mi.ramlan.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}

47e999b8499450a0b93e730337a7b47c42441548
fendryahyar <122244894+fendryahyar@users.noreply.github.com>
2023-01-10 02:28:24 +0800

    Pertemuan 5

---
+++ Pertemuan 5/SimpleCalcHomework_038/app/src/main/java/id/ac/poliban/mi/fendri/simplecalchomework_038/Calculator.java
@@ -0,0 +1,43 @@
+package id.ac.poliban.mi.fendri.simplecalchomework_038;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculator {
+    // Available operations
+    public enum Operator {ADD, SUB, DIV, MUL, POW}
+
+    /**
+     * Addition operation
+     */
+    public double add(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    /**
+     * Subtract operation
+     */
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    /**
+     * Divide operation
+     */
+    public double div(double firstOperand, double secondOperand) {
+        if (secondOperand == 0) throw new IllegalArgumentException("Can't divide with zero");
+        return firstOperand / secondOperand;
+    }
+
+    /**
+     * Multiply operation
+     */
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+
+    public double pow(double firstOperand, double secondOperand){
+        return Math.pow(firstOperand, secondOperand);
+    }
+}
